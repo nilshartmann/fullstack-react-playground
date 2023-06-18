c19dc927fa715974503b792dccb425baafbec392
Nils Hartmann <nils@nilshartmann.net>
2023-06-18 11:02:40 +0200

    remove old stuff

--- nextjs-hello-rsc-world/app/AppLink.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-type Args = Parameters<typeof Link>[0];
-
-export default function AppLink(p: Args) {
-  const className = "underline decoration-2 decoration-blue-700";
-
-  return <Link className={className} {...p} />;
-}
